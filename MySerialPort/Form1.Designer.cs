0402f6f18634e89a69471739d4bf99823717e97a
vkbc <veyselkabaci91@gmail.com>
2015-10-19 21:43:35 +0300

    v1.0

---
+++ MySerialPort/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace MySerialPort
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.PortName = "COM4";
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Gray;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Font = new System.Drawing.Font("Dotum", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.Color.DarkRed;
+            this.label1.Location = new System.Drawing.Point(332, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(135, 42);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(258, 229);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(382, 174);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(53, 183);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(240, 211);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox2.TabIndex = 5;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(12, 200);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(240, 207);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox3.TabIndex = 6;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Location = new System.Drawing.Point(646, 172);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(236, 187);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox4.TabIndex = 7;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Location = new System.Drawing.Point(646, 229);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(215, 192);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox5.TabIndex = 8;
+            this.pictureBox5.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Gray;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label2.Font = new System.Drawing.Font("Dotum", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.ForeColor = System.Drawing.Color.DarkRed;
+            this.label2.Location = new System.Drawing.Point(578, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(135, 42);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "label2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(973, 424);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ MySerialPort/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MySerialPort
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            pictureBox1.Location = new Point(300, 140);
+            pictureBox2.Location = new Point(0, 74);
+            pictureBox4.Location = new Point(675, 74);
+            pictureBox2.Controls.Add(pictureBox3);
+            pictureBox3.Location = new Point(0, 0);
+            pictureBox3.BackColor = Color.Transparent;
+            pictureBox4.Controls.Add(pictureBox5);
+            pictureBox5.Location = new Point(0, 0);
+            pictureBox5.BackColor = Color.Transparent;
+            serialPort1.Open();
+            serialPort1.DataReceived += serialPort1_DataReceived;
+        }
+
+        private void serialPort1_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+            string POT = serialPort1.ReadLine();
+            this.BeginInvoke(new LineReceivedEvent(LineReceived), POT);
+        }
+
+        private delegate void LineReceivedEvent(string POT);
+
+       private void LineReceived(string POT)
+       {
+
+           int voltaje,voltaje2;
+
+           string[] numbers = Regex.Split(POT, @"\D+");
+           foreach (string value in numbers)
+           {
+               if (!string.IsNullOrEmpty(value))
+               {
+                   int i = int.Parse(value);
+               }
+           }
+           
+           
+
+           if (Int32.TryParse(numbers[0], out voltaje))
+           {
+               voltaje = int.Parse(numbers[0]);
+               label1.Text = (voltaje ).ToString();
+           }
+           if (Int32.TryParse(numbers[1], out voltaje2))
+           {
+               voltaje2 = int.Parse(numbers[1]);
+               label2.Text = (voltaje2 ).ToString();
+           }
+
+           
+           Image Image = Image.FromFile("D:\\image\\car.png");
+           Image Image2 = Image.FromFile("D:\\image\\back_gray.png");
+           Image Image3 = Image.FromFile("D:\\image\\back1.png");
+           Image Image4 = Image.FromFile("D:\\image\\front1.png");
+           Image Image5 = Image.FromFile("D:\\image\\front_gray.png");
+
+
+           Bitmap Original2 = new Bitmap(Image2);
+           Bitmap TrasparentImage2 = new Bitmap(Image2.Width, Image2.Height);
+
+           Color c2 = Color.White;
+           Color v2 = Color.White;
+
+           for (int i = 0; i < Image2.Width; i++)
+           {
+               for (int y = 0; y < Image2.Height; y++)
+               {
+                   c2 = Original2.GetPixel(i, y);
+                   v2 = Color.FromArgb(255, c2.R, c2.G, c2.B);
+                   if (i > 300-(voltaje*3))
+                   {
+                       if (c2.R != 0 && c2.G != 0 && c2.B != 0)
+                       {
+                           TrasparentImage2.SetPixel(i, y, v2);
+                       }
+                   }
+
+               }
+           }
+
+           Bitmap Original5 = new Bitmap(Image5);
+           Bitmap TrasparentImage5 = new Bitmap(Image5.Width, Image5.Height);
+
+           Color c5 = Color.White;
+           Color v5 = Color.White;
+
+           for (int i2 = 0; i2 < Image5.Width; i2++)
+           {
+               for (int y2 = 0; y2 < Image5.Height; y2++)
+               {
+                   c5 = Original5.GetPixel(i2, y2);
+                   v5 = Color.FromArgb(255, c5.R, c5.G, c5.B);
+                   if (i2 < voltaje2*3)
+                   {
+                       if (c5.R != 0 && c5.G != 0 && c5.B != 0)
+                       {
+                           TrasparentImage5.SetPixel(i2, y2, v5);
+                       }
+                   }
+               }
+           }
+
+           pictureBox1.Image = Image;
+           pictureBox2.Image = Image3;
+           pictureBox3.Image = TrasparentImage2;
+           pictureBox4.Image = Image4;
+           pictureBox5.Image = TrasparentImage5;
+
+       }
+
+
+
+    }
+}
